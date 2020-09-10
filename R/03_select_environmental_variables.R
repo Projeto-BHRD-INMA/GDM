1c6eeec95dfcb55cb88c7e762882d5d9d6d71269
Talita Zupo <talita.zupo@gmail.com>
2020-09-10 14:49:13 -0300

    #pequenas alterações no script

--- R/03_select_environmental_variables.R
+++ R/03_select_environmental_variables.R
@@ -1,14 +1,17 @@
 if(!require(pacman)) install.packages("pacman")
 pacman::p_load(gdm, raster, maptools, rgdal, psych, plyr, devtools)
 
-tif <- list.files("GDM/Data/Abiotic/" , patt = ".tif")
+#preparando o raster com os dados abioticos; juntando as variaveis num raster só com várias camadas
+tif <- list.files("./Data/Abiotic/" , patt = ".tif")
 tif
 pres <- grep("30s", tif, value = T)
-setwd("GDM/Data/Abiotic/")
+setwd("GDM/Data/Abiotic/")#tem q estar dentro da pasta abiotic
+getwd()
 var_env <-stack(pres)
 plot(var_env[[1]])
-setwd("../../../")
+setwd("../../../") #volta diretorio
 
+#lendo shape da bacia e cropando o raster das variaveis ambientais para a bacia
 bacia <- readOGR("GDM/Data/Abiotic/munic_BHRD.shp")
 bacia <- spTransform(bacia, CRS("+proj=longlat +datum=WGS84"))
 env_crop <- crop(var_env, bacia)
@@ -22,7 +25,8 @@ env_v <- na.omit(env_v)
 
 
 
-######################correlacao
+####===============correlacao - pearson é default ============############
+#tambem writes tables para ver a correlaçao entre variaveis
 corr <- cor(env_v)
 abs(round(corr, 2)) # funçao abs, transforma em módulo ( tira o negativo)
 ifelse(corr >= 0.7, "sim", "nao")
@@ -31,12 +35,15 @@ write.table(abs(round(corr, 2)), "GDM/Results/Selected_variables/correlacao/cor_
 write.table(ifelse(corr >= 0.7, "sim", "nao"), "GDM/Results/Selected_variables/correlacao/cor_pres_afirmacao.xls",
             row.names = T, sep = "\t")
 
-##################VIF
+###============ VIF: Variance of inflation factor ===========####
+#outro jeito de determinar quais variaveis estao correlacionadas ou nao.
 
 
-source("GDM/R/vif_conc.R")
-vif<-vif_func(env_v[,-c(1:2)], thresh=10, trace=T)
+source("./R/vif_conc.R")
+vif<-vif_func(env_v[,-c(1:2)], thresh=10, trace=T) #usa a funçao vif_func (que está em outro script)
 vif
+#vif é uma lista das variaveis que nao estao correlacionadas; ou menos correlacionadas.
+
 # significado das bios
 # BIO01 = Temperatura media anual
 # BIO02 = Variacao da media diurna (media por mes (temp max - temp min))
@@ -59,19 +66,23 @@ vif
 # BIO19 = Precipitacao do trimestre mais frio
 
 vif
-lista <- c(2, 3, 5, 14, 18)
-setwd("GDM/Data/Abiotic/")
 
+#para selecionar as camadas correspondentes dessas variaveis do raster que contem todas as camadas - todas as variaveis e salvar um novo raster q contem so essas camadas que interessam.
+lista <- c(2, 3, 5, 14, 18)
+setwd("GDM/Data/Abiotic/") # de novo tem q estar na pasta abiotic
+getwd()
 for(i in lista){
   writeRaster(env[[i]], ifelse(i < 10, paste0("wc_bio0", i, ".tif"),
                                paste0("wc_bio", i, ".tif")),
               format = "GTiff", overwrite=TRUE)}
 
-tif <- list.files("GDM/Data/Abiotic/" , patt = ".tif")
+
+#tif <- list.files("./Data/Abiotic/", patt = ".tif") # ja estamos na abiotic...nao precisa aqui de novo
+tif <- list.files(patt = ".tif")
 wc <- grep("wc_bio", tif, value = T)
-setwd("GDM/Data/Abiotic/")
 wc <-stack(wc)
 plot(wc[[2]])
 writeRaster(wc,"wc_vif.grd", format = "raster", overwrite=TRUE)
-setwd("../../../")
+
+#setwd("../../../")
 
--- R/04_prepare_gdm_table.R
+++ R/04_prepare_gdm_table.R
@@ -2,11 +2,11 @@ if(!require(pacman)) install.packages("pacman")
 pacman::p_load(gdm, raster, maptools, rgdal, psych, plyr, devtools)
 
 
-bio <- read.delim("GDM/Data/Biotic/gbif_download.csv")
-bacia <- readOGR("GDM/Data/Abiotic/munic_BHRD.shp")
+bio <- read.delim("./Data/Biotic/gbif_download.csv")
+bacia <- readOGR("./Data/Abiotic/munic_BHRD.shp")
 bacia <- spTransform(bacia, CRS("+proj=longlat +datum=WGS84"))
 
-wc <-stack("GDM/Data/Abiotic/wc_vif.grd")
+wc <-stack("./Data/Abiotic/wc_vif.grd")
 plot(wc[[2]])
 
 bio<-bio[,c("decimalLongitude","decimalLatitude", "species")]
@@ -21,19 +21,31 @@ sum(dups2)
 data_tudo <- data_tudo[!dups2, ]
 data_tudo<-na.omit(data_tudo)
 
-png(filename="GDM/Results/map.png")
+#mapa das ocorrencias com uma das camadas das variaveis climpaticas, a camada [1] que é a bio_2.
+png(filename="GDM/Results/map1.png")
 plot(wc[[1]])
 points(data_tudo$lon, data_tudo$lat, pch = 20, cex=0.5)
 plot(bacia, add = T, border = "dark grey")
 dev.off()
 
+#pra ver as ocorrencias e as UCs####
+uc <- readOGR("./Data/UC_todas/crop_all_bhrd.shp")
+uc <- spTransform(uc, CRS("+proj=longlat +datum=WGS84"))
+
+png(filename="./Results/map_2.png")
+plot(wc[[1]])
+points(data_tudo$lon, data_tudo$lat, pch = 20, cex=0.5)
+plot(uc, add = T, border = "red")
+dev.off()
+
+### ==== para tirar especies da lista que so ocorrem uma vez ===== ####
 teste<-data_tudo
-lista<-count(teste$species)
+lista<-count(teste$species) #qual frequencia de ocorrencia de cada ssp
 b<-lista$freq > 1
-sp<- lista[!b, ]
+sp<- lista[!b, ] #lista ssp q ocorrem so uma vez
 s <- which(teste$species %in% sp$x)
-newdata<-teste[-s,]
-write.table(newdata,"GDM/Data/Biotic/species.csv", sep=";", dec=",",row.names = F)
+newdata<-teste[-s,] #cria nova tabela tirando as spp q ocorrem so uma vez
+write.table(newdata,"./Data/Biotic/species.csv", sep=";", dec=",",row.names = F)
 
 
 
